d7fd137d50bd3bc62001bb222e6d9a09bb1d79d2
YasserAhmedMoh <48395651+YasserAhmedMoh@users.noreply.github.com>
2024-04-23 19:49:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "hello world this is my first devops project"
+    return "hello world, its me again after edit -_- "
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
